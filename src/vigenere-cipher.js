4bbdc93ad164a4634858c771634e46a3d3b6ce41
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2022-10-15 18:54:46 +0300

    feat: completed vigenereCipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -22,16 +22,79 @@ const { NotImplementedError } = require('../extensions/index.js')
 class VigenereCipheringMachine {
   constructor(isDirect) {
     this.isDirect = isDirect
+    this.letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   }
 
-  encrypt() {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+  encrypt(string, key) {
+    if (arguments.length < 2 || typeof string !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!')
+    }
+    let res = []
+    key = key.toUpperCase()
+    let saveStr = string
+    string
+      .toUpperCase()
+      .split(' ')
+      .join('')
+      .split('')
+      .forEach((el, idx) => {
+        if (!this.letters.includes(el)) {
+          res.push(el)
+          return
+        }
+        let indexString = this.letters.indexOf(el)
+        let indexKey = this.letters.indexOf(key[idx % key.length])
+        console.log(indexString, indexKey, this.letters[(indexString + indexKey) % 26])
+        res.push(this.letters[(indexString + indexKey) % 26])
+      })
+    saveStr.split('').forEach((el, idx) => {
+      if (el === ' ') {
+        res.splice(idx, 0, ' ')
+      }
+    })
+    if (this.isDirect === undefined || this.isDirect === true) {
+      return res.join('')
+    } else {
+      return res.reverse().join('')
+    }
   }
 
-  decrypt() {
-    throw new NotImplementedError('Not implemented')
-    // remove line with error and write your code here
+  decrypt(string, key) {
+    if (arguments.length < 2 || typeof string !== 'string' || typeof key !== 'string') {
+      throw new Error('Incorrect arguments!')
+    }
+    let res = []
+    key = key.toUpperCase()
+    let saveStr = string
+    string
+      .toUpperCase()
+      .split(' ')
+      .join('')
+      .split('')
+      .forEach((el, idx) => {
+        if (!this.letters.includes(el)) {
+          res.push(el)
+          return
+        }
+        let indexString = this.letters.indexOf(el)
+        let indexKey = this.letters.indexOf(key[idx % key.length])
+        let diff = indexString - indexKey
+        if (indexString - indexKey < 0) {
+          diff = 26 + (indexString - indexKey)
+        }
+        res.push(this.letters[diff % 26])
+      })
+    saveStr.split('').forEach((el, idx) => {
+      if (el === ' ') {
+        res.splice(idx, 0, ' ')
+      }
+    })
+    console.log(res)
+    if (this.isDirect === undefined || this.isDirect === true) {
+      return res.join('')
+    } else {
+      return res.reverse().join('')
+    }
   }
 }
 
